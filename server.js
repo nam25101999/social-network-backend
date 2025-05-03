8b0e896b3ec7cf39c7c5d5b1976a04f5320416a6
KhanhLinh <linh105463@donga.edu.vn>
2025-05-03 14:16:12 +0700

    fix: update server.js to use ES modules

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Cấu hình dotenv
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3306;
 
+// Cấu hình static cho thư mục /uploads
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));// Cho phép truy cập ảnh avatar qua URL
 
+// Import routes
+import userRoutes from './routes/userRoutes.js';
+import friendRoutes from './routes/friendRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+import commentsRoutes from './routes/commentsRoutes.js';
 
-// Routes users
-const userRoutes = require('./routes/userRoutes');
+// Sử dụng routes
 app.use('/api/users', userRoutes);
-//friend
-const friendRoutes = require('./routes/friendRoutes');
 app.use('/api/friends', friendRoutes);
-//post
-const postRoutes = require('./routes/postRoutes');
 app.use('/api/posts', postRoutes);
-const commentsRoutes = require('./routes/commentsRoutes');
 app.use('/api', commentsRoutes);
 
 app.listen(PORT, () => {
